2c31f163239054354aef79d3f4730c111550fa71
Erwin Fernanda <fernandaerwin277@gmail.com>
2022-08-29 15:03:01 +0700

    Update Code

--- pages/02_Data Insight.py
+++ pages/02_Data Insight.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from tools import processing_data as pdt
 from PIL import Image
 import time
+import numpy as np
 
 # Title
 
@@ -22,58 +23,63 @@ well = st.selectbox("Please choice one!", ["L26E",
                                             "L26F",
                                             "L26G",
                                             "L26H"])
+file_name = "data/dataset" + "_" + well + ".csv"
+data = pd.read_csv(file_name, sep=",")
+column_data = np.asarray(data.columns.values[1:])
+column_data = np.insert(column_data, 8, "GG Temp Avg")
+
+column = st.selectbox("Please choice one part simulator!", column_data)
 
 # Monitoring Plot
 
 st.markdown("<svg width=\"705\" height=\"5\"><line x1=\"0\" y1=\"2.5\" x2=\"705\" y2=\"2.5\" stroke=\"black\" "
             "stroke-width=\"4\" fill=\"black\" /></svg>", unsafe_allow_html=True)
 
-file_name = "data/dataset" + "_" + well + ".csv"
-data = pd.read_csv(file_name, sep=",")
-column_data = data.columns.values
 data = pdt.create_data(data)
 # data.ewm(span=spacing).mean()
 
 data.dropna(inplace=True)
 
-data["TimeStamp"] = pd.to_datetime(data['TimeStamp'])
-data["Year"] = data['TimeStamp'].dt.year
-data["Quarter"] = data['TimeStamp'].dt.quarter
-data["Month"] = data['TimeStamp'].dt.month
-data["Day"] = data['TimeStamp'].dt.day
-data["Hours"] = data['TimeStamp'].dt.hour
-data["Minutes"] = data['TimeStamp'].dt.minute
+dataset = data.loc[:, ["TimeStamp",
+                       column]]
+dataset["TimeStamp"] = pd.to_datetime(dataset['TimeStamp'])
+dataset["Year"] = dataset['TimeStamp'].dt.year
+dataset["Quarter"] = dataset['TimeStamp'].dt.quarter
+dataset["Month"] = dataset['TimeStamp'].dt.month
+dataset["Day"] = dataset['TimeStamp'].dt.day
+dataset["Hours"] = dataset['TimeStamp'].dt.hour
+dataset["Minutes"] = dataset['TimeStamp'].dt.minute
 
-data_year = data.groupby('Year')
-data_quarter = data.groupby('Quarter')
-data_month = data.groupby('Month')
-data_day = data.groupby('Day')
+data_year = dataset.groupby('Year')
+data_quarter = dataset.groupby('Quarter')
+data_month = dataset.groupby('Month')
+data_day = dataset.groupby('Day')
 
 with st.spinner('Wait for it...'):
     time.sleep(5)
 
 fig = plt.figure(figsize=(15, 5))
-data_year["GG Temp Avg"].agg('mean').plot()
+data_year[column].agg('mean').plot()
 plt.xlabel('')
-plt.title('Temperature Average GG Combust by Year')
+plt.title(str('Data Insight ' + column + ' by Year'))
 st.pyplot(fig)
 
 fig = plt.figure(figsize=(15, 5))
-data_quarter["GG Temp Avg"].agg('mean').plot()
+data_quarter[column].agg('mean').plot()
 plt.xlabel('')
-plt.title('Temperature Average GG Combust by Quarter')
+plt.title(str('Data Insight ' + column + ' by Quarter'))
 st.pyplot(fig)
 
 fig = plt.figure(figsize=(15, 5))
-data_month["GG Temp Avg"].agg('mean').plot()
+data_month[column].agg('mean').plot()
 plt.xlabel('')
-plt.title('Temperature Average GG Combust by Month')
+plt.title(str('Data Insight ' + column + ' by Month'))
 st.pyplot(fig)
 
 fig = plt.figure(figsize=(15, 5))
-data_day["GG Temp Avg"].agg('mean').plot()
+data_day[column].agg('mean').plot()
 plt.xlabel('')
-plt.title('Temperature Average GG Combust by Day')
+plt.title(str('Data Insight ' + column + ' by Day'))
 st.pyplot(fig)
 
 st.markdown("<svg width=\"705\" height=\"5\"><line x1=\"0\" y1=\"2.5\" x2=\"705\" y2=\"2.5\" stroke=\"black\" "
